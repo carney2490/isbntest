51a304191658d3cbe0e68c7cf4cd52d2457f4342
JTong <eldertong@gmail.com>
2017-07-24 13:38:06 -0400

    second test passing

--- isbn_test.rb
+++ isbn_test.rb
@@ -4,8 +4,13 @@ require_relative "isbnchecker.rb"
 class TestIsbn <Minitest::Test
 	
     def test_that_a_array_is_returned_in_check_input_function
-        isbn_number = 1234567890
+        isbn_number = 123456789
 		assert_equal([], check_input(isbn_number))
+    end
+
+    def test_that_length_returns_10
+        isbn_number = 1234567890
+		assert_equal(10, check_input(isbn_number))
     end
 
 end
--- isbnchecker.rb
+++ isbnchecker.rb
@@ -1,3 +1,9 @@
 def check_input(isbn_number)
-[]
+    isbn_array = isbn_number.to_s.split('')
+    if isbn_array.count == 10
+        isbn_array.count
+    else
+        puts ".count != 10"
+        []
+    end
 end
